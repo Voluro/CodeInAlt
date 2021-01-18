f7b31080d3fbf936615eebc32cd51d6d29d6be67
rexillion <61246620+rexillion@users.noreply.github.com>
2021-01-18 09:37:24 +0200

    Update alt2py.py

--- alt2py.py
+++ alt2py.py
@@ -1,1 +1,17 @@
-
+print('welcome to codeInAlt')
+print('place the file you want to compile in the same directory as this file')
+a = input('exact file name without .alt: ')
+file = open(f'{a}.alt', 'r')
+file = file.read()
+ls = file.split(';\n')
+print(ls)
+am = []
+def value(inp):
+    if inp.split("/")[0] == 
+for i in ls:
+    if i != "":
+        am.append(i)
+print(am)
+for i in am:
+    if i.split()[1] == 'set':
+        print(f'{i.split()[0]} = {i.split()[2]}')
